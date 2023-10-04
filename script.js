e589c85456506878eaa57b0c44c919633d7f7b7e
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-04 22:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function volume_sphere() {
-    //Write your code here
-  
+    //Write your code hereevent.preventDefault(); // Prevent the form from submitting
+
+    // Get the radius value entered by the user
+    const radius = parseFloat(document.getElementById("radius").value);
+
+    // Check if the radius is a valid number
+    if (!isNaN(radius) && radius >= 0) {
+        // Calculate the volume of the sphere
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  event.preventDefault(); // Prevent the form from submitting
+
+    // Get the radius value entered by the user
+    const radius = parseFloat(document.getElementById("radius").value);
+
+    // Check if the radius is a valid number
+    if (!isNaN(radius) && radius >= 0) {
+        // Calculate the volume of the sphere
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
